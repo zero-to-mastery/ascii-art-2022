095fc5d31e4a3b46c4ebbb6dbadea64722e2d705
pritindra <pritindra.d@gmail.com>
2022-10-03 12:31:26 +0530

    edited command line arguments parser for ascii color

--- webapp/ascii_art.py
+++ webapp/ascii_art.py
@@ -1,20 +1,41 @@
 import os
-
+import time
 from flask import Flask, flash, request, redirect, render_template, url_for
 from PIL import Image
 from werkzeug.utils import secure_filename
-
+import warnings
 from make_art import convert_image_to_ascii
-
-UPLOAD_FOLDER = "./images"
+from pathlib import Path
+IMG_FOLDER = os.path.join('static', "IMG")
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * \
+    1024  # File can not be largr than 16 Mb
+app.config["UPLOAD_FOLDER"] = IMG_FOLDER
 
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def check_for_folder():
+    '''Basically it will check if there are already existing folder named images or not'''
+    if not Path.cwd().name == "webapp":
+        warnings.warn(
+            "\nPlease shift webapp directory for program to run functionally", DeprecationWarning)
+# If this if block is not supplied then python will create the IMG_FOLDER directory outside and cause conflictions.
+    if Path.cwd().name == "webapp":
+        if os.path.isdir("static"):
+            return print("Existing Directories Found.")
+        for dirs in [IMG_FOLDER]:
+            if not os.path.isdir(dirs):
+                os.makedirs(dirs, mode=777)
+            print("Initializig Directories...")
+
+
+check_for_folder()
+# Giving user time to read the message provided by the fucntion above
+time.sleep(4)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -34,5 +55,17 @@ def upload_file():
             file.save(filepath)
             img = Image.open(filepath)
             ascii_ = convert_image_to_ascii(img)
+            file.close()
             return render_template("ascii.html", ascii=ascii_)
     return render_template("upload.html")
+
+
+@app.route("/gallery")
+def gallery():
+    IMG_LIST = os.listdir('static/IMG')
+    IMG_LIST = ['IMG/' + i for i in IMG_LIST]
+    return render_template("gallery.html", imagelist=IMG_LIST)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
