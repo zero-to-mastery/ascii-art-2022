f934ffae5bbfe2601934726687b65b3b00e3026d
Talha Quddoos <talhaquddoos1@gmail.com>
2022-10-21 23:59:10 +0500

    Added background color support

--- webapp/static/text-art.js
+++ webapp/static/text-art.js
@@ -238,3 +238,8 @@ function downloadArt() {
 }
 
 document.querySelector('.wrapper-dropdown').addEventListener('click', toggleDropdown);
+
+
+document.querySelector("#background-color").addEventListener('input', () => {
+    document.querySelector('.ascii-art').style.backgroundColor = document.querySelector("#background-color").value;
+})
